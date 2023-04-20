414b8c0596866b6672bd9641f5fa9f55770323de
Katelyn Schreyer <krschreyer@gmail.com>
2023-04-20 13:29:58 -0400

    MVP works
    - Allows for single-digit range operands
    - Downloads work (but aren't pretty)

--- math-races/app.R
+++ math-races/app.R
@@ -1,16 +1,17 @@
 ## CORE LOGIC ##
 
 # Addition
-addition_problem <- function(range1, range2) {
-  operand1 <- sample(range1,1)
-  operand2 <- sample(range2,1)
+
+addition_problem <- function(range1 = 1:5, range2 = 1:5) {
+  operand1 <- ifelse(length(range1) == 1, range1, sample(range1,1))
+  operand2 <- ifelse(length(range2) == 1, range2, sample(range2,1))
   paste(operand1, "+", operand2, "=")
 }
 
 # Subtraction
-subtraction_problem <- function(range1, range2, allow_negatives = F) {
-  operand1 <- sample(range1,1)
-  operand2 <- sample(range2,1)
+subtraction_problem <- function(range1 = 1:5, range2 = 1:5, allow_negatives = F) {
+  operand1 <- ifelse(length(range1) == 1, range1, sample(range1,1))
+  operand2 <- ifelse(length(range2) == 1, range2, sample(range2,1))
   
   if (all(!allow_negatives, operand2 > operand1)) {
     while (operand2 > operand1) {
@@ -57,12 +58,14 @@ ui <- fluidPage(
                         label = "Max Value for Operand 2",
                         min = 0,
                         max = 10,
-                        value = 5)
+                        value = 5),
+            actionButton("generate", "Generate Problems")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           htmlOutput("problem_list")
+           htmlOutput("problem_list"),
+           downloadButton("download", "Download Printable")
         )
     )
 )
@@ -70,15 +73,39 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$problem_list <- renderUI({
-       
-      if (input$operation == "Addition") {
-      problems <- replicate(20, addition_problem(range1 = input$range1start:input$range1end, range2 = input$range2start:input$range2end))
-      } else if (input$operation == "Subtraction") {
-      problems <- replicate(20, subtraction_problem(range1 = input$range1start:input$range1end, range2 = input$range2start:input$range2end))
-      }
-      HTML(paste(problems, collapse = "<br>", sep = ""))
+  range1 <- eventReactive(input$generate, {
+    input$range1start:input$range1end
+  })
+  
+  range2 <- eventReactive(input$generate, {
+    input$range2start:input$range2end
+  })
+  
+  problem_list <- reactive({
+    
+    if (input$operation == "Addition") {
+      problems <- replicate(20, addition_problem(range1 = range1(), range2 = range2()))
+    } else if (input$operation == "Subtraction") {
+      problems <- replicate(20, subtraction_problem(range1 = range1(), range2 = range2()))
+    }
+  })
+  
+  output$problem_list <- renderUI({
+      
+      HTML(paste(problem_list(), collapse = "<br>", sep = ""))
     })
+  
+  output$download <- downloadHandler(
+    filename = "worksheet.html",
+    content = function(file) {
+      params <- list(n = problem_list())
+      rmarkdown::render("worksheet.Rmd", 
+                        output_file = file,
+                        params = params,
+                        envir = new.env(parent = globalenv()))
+                        }
+    
+  )
 }
 
 # Run the application 
